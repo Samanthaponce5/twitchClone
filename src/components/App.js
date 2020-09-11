b6d9d853cbe2733a7e9d9eec64bb38b9df3927b0
Samanthaponce5 <samanthaponce5@gmail.com>
2020-09-11 12:09:39 -0400

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import StreamCreate from '../streams/StreamCreate'
+import StreamEdit from '../streams/StreamEdit'
+import StreamDelete from '../streams/StreamDelete'
+import StreamList from '../streams/StreamList'
+import StreamShow from '../streams/StreamShow'
 
 const App=()=>{
 
     return(
-        <h3>Heller</h3>
+        <BrowserRouter>
+        <div>
+            <Route path='/' exact component={StreamList}/>
+            <Route path='/streams/new' exact component={StreamCreate}/>
+            <Route path='/streams/edit' exact component={StreamEdit}/>
+            <Route path='/stream/delete' exact component={StreamDelete}/>
+            <Route path='/stream/show' exact component={StreamShow}/>
+        </div>
+        </BrowserRouter>
     )
 }
 
