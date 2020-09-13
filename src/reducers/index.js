ffdb6ea995c36c75ed06b56916d958d4b3cd02d2
Samanthaponce5 <samanthaponce5@gmail.com>
2020-09-13 13:21:03 -0400

    reduxform

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import authReducer from './AuthReducer'
+import {reducer} from 'redux-form'
 
 export default combineReducers({
-    auth:authReducer
+    auth:authReducer,
+    form:reducer
 })
--- src/streams/StreamCreate.js
+++ src/streams/StreamCreate.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Field,reduxForm } from 'redux-form'
 
 
-const StreamCreate=()=>{
+class StreamCreate extends React.Component{
+
+    renderInput=(formProps)=>{
+       return <input value={formProps.input.value} onChange={formProps.input.onChange}/>
+
+       //or you can also do it this way by using the spread 
+       //operator basically putting all the props into the input
+       //which is recommended since there more props to be used
+       //than just value and onchange 
+       //<input {...formProps.input}/>
+    }
+    render(){
     return(
-        <div>Stream Create</div>
+       <form>
+       <Field name='title' component={this.renderInput}/>
+       <Field name='description' component={this.renderInput}/>
+       </form>
     )
 }
+}
 
-export default StreamCreate
+export default reduxForm(
+    {
+        form:'streamCreate'
+    }
+)(StreamCreate)
